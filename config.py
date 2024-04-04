ddae600f8ac80a7d62cd5d57dccc16452da4d9f9
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-04-04 11:37:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 999999))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @emma_x_robot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6814183827))
+OWNER_ID = int(getenv("OWNER_ID", 6787625019))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -35,15 +35,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THEMADMAXPRO/MadmaxXMusic",
+    "https://github.com/THEMADMAXOP/LUCIFER",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "MadmaxXMusic")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/STATUSDAIRY2")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VOICEOFHEART0")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ABOUT_LUCIFER0/3")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Lucifer_x_fed")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -81,10 +81,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/3eaaee3b5cfbc0e42f2f3.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/0ea046bf93746ec67d410.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/8a0508262e794c96a3e48.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/0ea046bf93746ec67d410.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
